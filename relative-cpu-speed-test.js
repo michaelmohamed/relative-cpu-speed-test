704d830efae4240e4868391ade607e55284370e5
Michael Mohamed <3814596+mm-fsai@users.noreply.github.com>
2023-05-04 23:05:24 -0400

    Create relative-cpu-speed-test.js

---
+++ relative-cpu-speed-test.js
@@ -0,0 +1,14 @@
+function testRelativeSpeed(testSeed, testMs) {
+  return new Promise((resolve, reject) => {
+    const start = performance.now();
+    let result = 0;
+    for (let i = Math.pow(testSeed, 7); i >= 0; i--) {
+      result += Math.atan(i) * Math.tan(i);
+    }
+    const end = performance.now();
+    const timeMs = Math.round(end - start);
+    const passed = timeMs <= testMs;
+    console.log(`The CPU Speed is ${timeMs} milliseconds.`)
+    passed ? resolve([testSeed, timeMs]) : reject([testSeed, timeMs]);
+  });
+}
